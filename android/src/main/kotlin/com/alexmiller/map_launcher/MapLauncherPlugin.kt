68e33c63a9aa85c8f635a114e311dd9729499703
Ondřej Boura <ondraboura@gmail.com>
2022-01-25 14:46:43 +0100

    Added missing mapyCz MapType on Android

--- android/src/main/kotlin/com/alexmiller/map_launcher/MapLauncherPlugin.kt
+++ android/src/main/kotlin/com/alexmiller/map_launcher/MapLauncherPlugin.kt
@@ -11,7 +11,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-private enum class MapType { google, googleGo, amap, baidu, waze, yandexNavi, yandexMaps, citymapper, mapswithme, osmand, osmandplus, doubleGis, tencent, here }
+private enum class MapType { google, googleGo, amap, baidu, waze, yandexNavi, yandexMaps, citymapper, mapswithme, osmand, osmandplus, doubleGis, tencent, here, mapyCz }
 
 private class MapModel(val mapType: MapType, val mapName: String, val packageName: String) {
     fun toMap(): Map<String, String> {
