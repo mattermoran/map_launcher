859deace74cdffe157c8085bc08d6cbf613c4cb2
EvGeniyLell <evgeniylell@gmail.com>
2024-08-07 03:44:05 +0300

    Update build HERE route

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -13,12 +13,6 @@ class Utils {
         .firstWhere((type) => type.toString().split('.').last == value);
   }
 
-  // static String? nullOrValue(dynamic nullable, String value) {
-  //   if (nullable == null) return null;
-  //   return value;
-  // }
-
-
   /// Constructs a url from [url] and [queryParams]
   static String buildUrl({
     required String url,
